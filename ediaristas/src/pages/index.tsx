19341223b6c3a1fad21663dba717811b1b67ba60
GeovanneAtanazio <geovanne.atanazio@dcomp.ufs.br>
2021-10-16 21:37:38 -0300

    Criação do componente TextField

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,9 +1,14 @@
-import type { NextPage } from 'next';
-import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
-import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
-import UserInformaton from 'ui/components/data-display/UserInformation/UserInformation';
-
-
+import type { NextPage } from "next";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformaton from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography, Container } from "@mui/material";
+import {
+  FormElementsContainer,
+  ProfissionaisPaper,
+  ProfissionaisContainer
+} from "@styles/pages/index.style";
 
 const Home: NextPage = () => {
   return (
@@ -15,14 +20,66 @@ const Home: NextPage = () => {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       />
-      <UserInformaton 
-        name={'Geovanne'} 
-        picture={'https://github.com/GeovanneAtanazio.png'}
-        rating={3}
-        description={'Sergipe'}
-      />
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={"99.999-999"}
+            label={"Digite seu CEP"}
+            variant={"outlined"}
+            fullWidth
+          />
+          <Typography color={"error"}>CEP inválido</Typography>
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformaton
+              name={"Geovanne"}
+              picture={"https://github.com/GeovanneAtanazio.png"}
+              rating={3}
+              description={"Sergipe"}
+            />
+            <UserInformaton
+              name={"Geovanne"}
+              picture={"https://github.com/GeovanneAtanazio.png"}
+              rating={3}
+              description={"Sergipe"}
+            />
+            <UserInformaton
+              name={"Geovanne"}
+              picture={"https://github.com/GeovanneAtanazio.png"}
+              rating={3}
+              description={"Sergipe"}
+            />
+            <UserInformaton
+              name={"Geovanne"}
+              picture={"https://github.com/GeovanneAtanazio.png"}
+              rating={3}
+              description={"Sergipe"}
+            />
+            <UserInformaton
+              name={"Geovanne"}
+              picture={"https://github.com/GeovanneAtanazio.png"}
+              rating={3}
+              description={"Sergipe"}
+            />
+            <UserInformaton
+              name={"Geovanne"}
+              picture={"https://github.com/GeovanneAtanazio.png"}
+              rating={3}
+              description={"Sergipe"}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
